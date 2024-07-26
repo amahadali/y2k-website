f93bc4fb050ccfc165990e5bc9b9cafde500fc72
abdi <amahadali1@hotmail.com>
2024-07-26 18:13:31 +0100

    errorless button profile

--- src/app/components/Feed/Feed.tsx
+++ src/app/components/Feed/Feed.tsx
@@ -177,7 +177,7 @@ const Feed: React.FC<FeedProps> = ({
       </Masonry>
 
       <div className="mt-6 text-center">
-        <Link href={`/profile/${session.user.username}`}>
+        <Link href={`/profile/${session?.user.username}`}>
           <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
             View Profile
           </button>
